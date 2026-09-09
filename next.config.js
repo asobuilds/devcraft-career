618c7151d7879c2bcc425bf1d07e61517ae14c00
asobuilds <agenesunday143@gmail.com>
2026-09-09 16:06:26 +0100

    fix: rename configuration channel to standard next.config.js format

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   images: {
-    unoptimized: true, // Bypasses explicit image hostname checks to allow rapid deployment uploads
+    unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
